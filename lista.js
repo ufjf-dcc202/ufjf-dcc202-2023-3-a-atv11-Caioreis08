4e57da3ccf6658db60d6fbc2a29836eada04595c
Caio Reis <fernandes.caio@estudante.ufjf.br>
2023-11-29 20:54:38 -0300

    listas.js foi alterada

--- lista.js
+++ lista.js
@@ -4,4 +4,12 @@ export function getLista() {
     return lista;
 }
 
+export function limparLista(){
+    lista.splice(0);
+}
+
+export function adicionarNaLista(novoItem){
+    lista.push(novoItem);
+}
+
 // export {getLista};
