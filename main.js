7e2d912f2cc1028afc0adaee90d015b4f5a69699
Caio Reis <fernandes.caio@estudante.ufjf.br>
2023-11-29 20:25:33 -0300

    main.js foi alterada

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // main.js
+import { getLista } from "./lista.js";
 
 // Passo 1 - Ache os elementos importantes
 const pEntrada = document.querySelector('#entrada');
@@ -6,4 +7,16 @@ const olSaida = document.querySelector('#saida');
 const btnAdicionar = document.querySelector('#adicionar');
 const btnLimpar = document.querySelector('#limpar');
 
-olSaida.innerText = "<h1>Boo!</h1>";
+atualizarItensDeLista();
+atualizarItensDeLista();
+
+function atualizarItensDeLista (params) {
+    olSaida.innerHTML = "";
+    const lista = getLista();
+    for (let i = 0; i < lista.lengt; i++) {
+        const item = lista[i];
+        const li = document.createElement('li');
+        li.textContent = item;
+        olSaida.appendChild(li);
+    }
+}
